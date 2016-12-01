536f08ef024baaf0077078212bb4e79d97019fed
zhengchaoCredit <904100923@qq.com>
2016-12-01 10:35:29 +0800

    遍历二叉树

---
+++ src/erchashu_bianli/Node.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package erchashu_bianli;
+
+/**
+ *
+ * @author zhengchao
+ */
+public class Node {
+    
+    public int value;
+    
+    public Node left;
+    
+    public Node right;
+    
+    public  Node(int value){
+        this.value = value;
+    }
+    
+}
