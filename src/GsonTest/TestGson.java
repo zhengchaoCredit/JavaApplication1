d8ce8b2bcc53c3351fdbcfdb188d0a8f8602c472
zhengchaoCredit <904100923@qq.com>
2016-12-23 10:17:28 +0800

    Gson

---
+++ src/GsonTest/TestGson.java
@@ -0,0 +1,25 @@
+//package GsonTest;
+//
+//import com.google.gson.Gson; 
+//
+///**
+// *
+// * @author zhengchao
+// */
+//public class TestGson {
+//    
+//    public static void main(String[] args){
+//        
+//        
+//        Gson gson = new Gson();  
+//        
+//        User user=new User("zhengchao",25);
+//        
+//        String userJson = gson.toJson(user);  
+//        
+//        User user1 = gson.fromJson(userJson, User.class);  
+//        
+//        
+//    }
+//    
+//}
---
+++ src/GsonTest/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package GsonTest;
+
+/**
+ *
+ * @author zhengchao
+ */
+public class User {
+    
+    private String name;
+    
+    private int age;
+    
+    public User(String name,int age){
+        
+        this.name = name;
+        this.age = age;
+    }
+    
+}
